5ef54d717013dc611ed13edda2262363e6e70fee
xtenzin <code@xtenzin.com>
2026-02-04 19:11:58 +0800

    更新版本号至 0.1.1
    ---
    Update version to 0.1.1

--- src/constants/version.ts
+++ src/constants/version.ts
@@ -1,6 +1,6 @@
 // 版本信息常量
 // Version information constants
-export const APP_VERSION = '0.1.0'
+export const APP_VERSION = '0.1.1'
 export const COPYRIGHT_YEAR = 2026
 export const AUTHOR = 'xtenzin'
 export const AUTHOR_EMAIL = 'code@xtenzin.com'
