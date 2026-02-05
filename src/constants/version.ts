7bd460e2945fff28b7012798061c13eeb6e20a78
xtenzin <code@xtenzin.com>
2026-02-05 10:14:33 +0800

    chore: 更新版本号至 v0.2.1
    版本更新：
    - 版本号从 0.2.0 升级到 0.2.1
    
    ---
    
    chore: Update version to v0.2.1
    
    Version update:
    - Version upgraded from 0.2.0 to 0.2.1

--- src/constants/version.ts
+++ src/constants/version.ts
@@ -1,6 +1,6 @@
 // 版本信息常量
 // Version information constants
-export const APP_VERSION = '0.2.0'
+export const APP_VERSION = '0.2.1'
 export const COPYRIGHT_YEAR = 2026
 export const AUTHOR = 'xtenzin'
 export const AUTHOR_EMAIL = 'code@xtenzin.com'
